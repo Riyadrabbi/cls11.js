54928fd138f65bc68be1b64f42722f817706afad
Riyadrabbi <riyadrabbi904@gmail.com>
2023-10-05 20:50:54 +0600

    first commit

---
+++ cls11.js
@@ -0,0 +1,61 @@
+// let studentRoll = [480229, 480228, 480227, 480224, 480221];
+// studentRoll.push(480248);
+// studentRoll.unshift(480222);
+// studentRoll.unshift(480223);
+// let flowerName = ['lily', 'Rose', 'Iris', 'Jasmine', ];
+// flowerName.pop();
+// flowerName.shift();
+
+
+
+// while
+// let r = 1;
+// while (r < 10) {
+//     console.log('Condition fulfilled');
+//     r++;
+
+// }
+
+// again while
+
+// let i = 1;
+// while (i < 10) {
+//     console.log('Condition faw')
+//     i++;
+
+// }
+
+
+// do while
+
+// let i = 0;
+// do {
+//     console.log('Hey Normal people')
+//     i++;
+
+// } while (i < 10);
+
+// for while
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i * 8);
+
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i * 5);
+// 
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i, '*', 3, '=', (i * 3))
+}
+
+
+
+
+
+
+
+// console.log(studentRoll);
+// console.log('MY ROLL', studentRoll[0]);
+// console.log(flowerName);
---
+++ practise.js
@@ -0,0 +1,84 @@
+let nme = 'Riyad Rabbi';
+let age = 20;
+let semesterRoll = 480229;
+let my_department = 'Coomputer';
+
+let numbr1 = 80;
+let numbr2 = 40;
+let numbr3 = 10;
+let bksh = 1200;
+bksh = bksh - 500;
+bksh = bksh + 10;
+let nagad = 10000;
+nagad += 500;
+let firstName = 'Riyad';
+let lastName = 'Rabbi';
+let fullName = firstName + " " + lastName;
+
+let mark = '20';
+let mark2 = '290';
+let mark3 = 100;
+let value = mark + mark2 + mark3
+let convert = parseInt(mark)
+let conver = parseInt(mark2)
+
+
+let ri = 0.2;
+let Ri = 0.3;
+
+let a = 0.1;
+let b = 0.2;
+let sum2 = a + b;
+
+
+
+let aa = 40;
+let bb = 60;
+if (aa == bb) {
+    console.log('Wow good')
+} else {
+    console.log('Thats not good');
+}
+let c = 190;
+let d = 170;
+if (c => d) {
+    console.log('Awesom');
+} else {
+    console.log('OOH NOP');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log('My Name =', nme);
+console.log('My age :', age);
+console.log('My Semester Roll :', semesterRoll);
+console.log('My Department', my_department);
+console.log(numbr1 - numbr2);
+console.log(numbr1 * numbr2);
+console.log(numbr1 + numbr2);
+console.log('My Total Ammount :', bksh);
+console.log('My Nagad', nagad);
+console.log(fullName);
+console.log(value);
+console.log(convert);
+console.log(conver);
+console.log(Ri + ri)
+console.log(a + b);
+console.log(sum2.toFixed(2));
