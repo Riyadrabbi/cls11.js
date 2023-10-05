dc23e218982f3a7baf8f80c9105002a4741e0cd3
Rayad Hossain Rabbi <143387247+Riyadrabbi@users.noreply.github.com>
2023-10-05 18:56:07 +0600

    Add files via upload

---
+++ cls12.js
@@ -0,0 +1,28 @@
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i, 'x', 4, '=', (i * 4))
+// }
+
+// for (let i = 1; i <= 1000; i++) {
+//     for (let j = 1; j <= 10; j++) {
+//         console.log(i, 'x', j, '=', (i * j))
+//     }
+// }
+
+
+// for (let i = 1; i <= 30; i++) {
+//     if (i == 10) {
+//         break;
+
+//     }
+
+//     for (let j = 1; j <= 10; j++) {
+//         console.log(i, 'x', j, '=', (i * j))
+//     }
+
+// }
+let mobileDev = { Name: 'Realme 9se', Brand: 'Realme', Ram: '8Gb', Rom: '128GB', Price: 24000, };
+
+// console.log(mobileDev)
+
+console.log('Price =', mobileDev.Price)
+console.log(mobileDev['Brand']);
